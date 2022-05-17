0f84d32e3561b340a1d14aa697c84923b567a34e
Linnkid_CHEN <linnkid.chen@gmail.com>
2022-05-17 13:19:38 +0800

    may17

---
+++ simos/interrupt/Interrupt.cpp
@@ -0,0 +1,34 @@
+#include "Interrupt.h"
+void interrupt::add_itrpt(int process_num , ITRTYPE itr_type , int deviceInfo,memory::Pointer<char> ptr=nullptr ) {
+    itrpt_que.emplace(process_num, itr_type, deviceInfo);
+    itrpt_switch = true;
+    
+}
+void interrupt:: process() {
+    if (itrpt_que.empty()) {
+        itrpt_switch = false;
+        return;
+    }
+    while (!itrpt_que.empty()) {
+        work(itrpt_que.front());
+        itrpt_que.pop();
+    }
+    itrpt_switch = false;
+}
+
+void interrupt::work(intVec& Input) {
+    if (!valid(Input))
+        return;
+    if (Input.itrpt_type == input) {
+        //call keyboard input
+    }
+    if (Input.itrpt_type == output) {
+        //call keyboard output
+    }
+}
+
+bool interrupt::valid(const intVec &input) {
+    if (input.device_num >= 0 && input.itrpt_type != none && input.process >= 0)
+        return true;
+    else return false;
+}
---
+++ simos/interrupt/Interrupt.h
@@ -0,0 +1,59 @@
+#ifndef INTERRUPT_H
+#define INTERRUPT_H
+#include <queue>
+#if __cplusplus < 201103L
+	#error "This program requires C++ 11 or later"
+#    endif
+
+#    include <iostream>
+
+
+// 应在初始化时生成一个唯一的interrupt类
+// 请使用interrupt::getInstance()调用public函数
+// 如：interrupt::getInstance().check_switch
+//
+class interrupt {
+    enum ITRTYPE { input, output, soft, none = 0 };//TODO 讨论中断类型
+class intVec {
+   
+public:
+    intVec(int processnum=-1,ITRTYPE itrtype=none, int device=-1):process(processnum),itrpt_type(itrtype),device_num(device){}    
+     int process; //进程号
+    ITRTYPE itrpt_type = none;
+    int device_num=-1;
+    };
+    private:
+    static std::queue<intVec> itrpt_que;
+    static bool                itrpt_switch; //中断开关
+    interrupt() =default;
+	~interrupt() = default;
+	interrupt(const interrupt&);
+	interrupt& operator=(const interrupt&);
+        bool       valid(const intVec &input);
+        void work(intVec& input);
+public:
+    bool check_switch() { return itrpt_switch; }
+    int check_que_len(){return itrpt_que.size();}
+    void add_itrpt(int process_num = -1, ITRTYPE itr_type = none,
+                   int deviceInfo = 0,memory::Pointer<char> ptr=nullptr);
+    void process();//固定时间执行，检查是否有中断需要处理。如果有，执行中断处理程序；如果没有，跳过。
+
+    static interrupt &getInstance() {
+        static interrupt instance;
+        return instance;
+    }
+}
+;
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
