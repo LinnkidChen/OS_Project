5ecca7bfda88de665af5995ceca7c9bdf925dfde
Deemoink <96469350+Deemoink@users.noreply.github.com>
2022-05-20 21:43:14 +0800

    Update DeviceManager.h

--- simos/DeviceManagement/DeviceManager.h
+++ simos/DeviceManagement/DeviceManager.h
@@ -13,7 +13,7 @@
 
 #include "dvmainwindow.h"
 
-extern DVMainWindow* w;
+extern DVMainWindow *w;
 
 //******************************外部接口******************************
 
@@ -33,86 +33,102 @@ public:
     static std::map<int, Device>
     getDeviceList(DeviceType deviceType); //获取设备列表
 
+    static int freeDevice(deviceID); //将设备状态设为空闲（设备ID）
+    // 0，设置失败：设备ID不存在
+    // 1，设置成功
+
     //--------------------驱动接口--------------------
 
-    static int disk_write(int         address,
+    static int disk_write(int address,
                           std::string content); //磁盘写（地址，内容）
     static int disk_write(int deviceID, int address,
                           std::string content); //磁盘写（设备ID,地址，内容）
-    // -1，没有磁盘设备
-    // 0，磁盘繁忙
-    // 1，存储成功
-    static std::string disk_read(int address,
-                                 int size_byte); //磁盘读（地址，内容）
-    static std::string disk_read(int deviceID, int address,
-                                 int size_byte); //磁盘读（设备ID,地址，内容）
-    // ""，没有磁盘设备
-    static char keyboard_read();             //获取键盘输入
-    static char keyboard_read(int deviceID); //获取键盘输入(设备ID)
-    // '\0'，没有键盘设备
+
+    static int disk_read(int address,
+                         int size_byte); //磁盘读（地址，内容）
+    static int disk_read(int deviceID, int address,
+                         int size_byte); //磁盘读（设备ID,地址，内容）
+
+    static int keyboard_read();             //获取键盘输入
+    static int keyboard_read(int deviceID); //获取键盘输入(设备ID)
+
     static int printer_write(std::string content); //打印机打印（内容）
     static int printer_write(int deviceID,
                              std::string content); //打印机打印（设备ID,内容）
-    // -1，没有打印机设备
-    // 0，打印机繁忙
-    // 1，打印成功
+
     static int terminal_write(std::string content); //终端打印（内容）
-    static int terminal_write(int         deviceID,
+    static int terminal_write(int deviceID,
                               std::string content); //终端打印（设备ID,内容）
-    // -1，没有终端设备
-    // 0，终端繁忙
-    // 1，打印成功
+    // -1，没有磁盘设备
+    // -2，磁盘繁忙
+    // other，设备ID<存储成功>
 };
 
 //******************************设备管理******************************
 
-class DeviceManager : public DeviceDict {
+class DeviceManager : public DeviceDict
+{
 private:
-    static int                   deviceIDPool; //设备ID分配池
-    static std::map<int, Device> deviceList;   //设备列表
+    static int deviceIDPool;                 //设备ID分配池
+    static std::map<int, Device> deviceList; //设备列表
 
 public:
-    static Device getDevice(int deviceID) {
-        if (deviceList.count(deviceID) == 1) {
+    static Device getDevice(int deviceID)
+    {
+        if (deviceList.count(deviceID) == 1)
+        {
             return deviceList[deviceID];
         }
     }
 
-    static int addDevice(DeviceType deviceType) {
+    static int addDevice(DeviceType deviceType)
+    {
         int deviceID = -1;
         //为新设备分配ID
-        if (deviceList.size() < INT_MAX) {
-            if (deviceList.size() == deviceIDPool) {
+        if (deviceList.size() < INT_MAX)
+        {
+            if (deviceList.size() == deviceIDPool)
+            {
                 deviceID = deviceIDPool + 1;
                 deviceIDPool++;
-            } else {
-                for (int i = 0; i < deviceIDPool; i++) {
-                    if (deviceList.count(i) == 0) {
+            }
+            else
+            {
+                for (int i = 0; i < deviceIDPool; i++)
+                {
+                    if (deviceList.count(i) == 0)
+                    {
                         deviceID = i;
                         break;
                     }
                 }
             }
-        } else
+        }
+        else
             return 0;
         //创建新设备
-        switch (deviceType) {
-        case DeviceType::disk: {
+        switch (deviceType)
+        {
+        case DeviceType::disk:
+        {
             Device_Disk disk(deviceID);
             deviceList[deviceID] = disk;
             break;
         }
-        case DeviceType::keyboard: {
+        case DeviceType::keyboard:
+        {
             Device_Keyboard keyboard(deviceID);
             deviceList[deviceID] = keyboard;
             break;
         }
-        case DeviceType::printer: {
+        case DeviceType::printer:
+        {
             Device_Printer printer(deviceID);
             deviceList[deviceID] = printer;
             break;
         }
-        case DeviceType::terminal: {
+        case DeviceType::terminal:
+        {
             Device_Terminal terminal(deviceID);
             deviceList[deviceID] = terminal;
             break;
@@ -125,10 +141,14 @@ public:
         return 1;
     }
 
-    static int removeDevice(int deviceID) {
-        if (deviceList.count(deviceID) == 1) {
-            if (deviceList[deviceID].getState() != DeviceStateType::busy) {
-                switch (deviceList[deviceID].getType()) {
+    static int removeDevice(int deviceID)
+    {
+        if (deviceList.count(deviceID) == 1)
+        {
+            if (deviceList[deviceID].getState() != DeviceStateType::busy)
+            {
+                switch (deviceList[deviceID].getType())
+                {
                 case DeviceType::disk:
                     Device_Disk::removeDevice(deviceID);
                     break;
@@ -145,14 +165,17 @@ public:
                 deviceList.erase(deviceID);
                 w->UpdateContents(w);
                 return 1;
-            } else
+            }
+            else
                 return 0;
         }
         return -1;
     }
 
-    static std::map<int, Device> getDeviceList(DeviceType deviceType) {
-        switch (deviceType) {
+    static std::map<int, Device> getDeviceList(DeviceType deviceType)
+    {
+        switch (deviceType)
+        {
         case DeviceType::all:
             return deviceList;
             break;
@@ -171,33 +194,51 @@ public:
         }
     }
 
-    static int disk_write(int address, std::string content) {
+    static int freeDevice(int deviceID)
+    {
+        if(deviceList.count(deviceID) == 1)
+        {
+            deviceList[deviceID].setSate(DeviceDict::DeviceStateType::free);
+        }
+        return 0;
+    }
+
+    static int disk_write(int address, std::string content)
+    {
         return Device_Disk::disk_write(address, content);
     }
-    static std::string disk_read(int address, int size_byte) {
+    static int disk_read(int address, int size_byte)
+    {
         return Device_Disk::disk_read(address, size_byte);
     }
-    static char keyboard_read() { return Device_Keyboard::keyboard_read(); }
-    static int  printer_write(std::string content) {
+    static int keyboard_read() { return Device_Keyboard::keyboard_read(); }
+    static int printer_write(std::string content)
+    {
         return Device_Printer::printer_write(content);
     }
-    static int terminal_write(std::string content) {
+    static int terminal_write(std::string content)
+    {
         return Device_Terminal::terminal_write(content);
     }
 
-    static int disk_write(int deviceID, int address, std::string content) {
+    static int disk_write(int deviceID, int address, std::string content)
+    {
         return Device_Disk::disk_write(deviceID, address, content);
     }
-    static std::string disk_read(int deviceID, int address, int size_byte) {
+    static int disk_read(int deviceID, int address, int size_byte)
+    {
         return Device_Disk::disk_read(address, size_byte);
     }
-    static char keyboard_read(int deviceID) {
+    static int keyboard_read(int deviceID)
+    {
         return Device_Keyboard::keyboard_read(deviceID);
     }
-    static int printer_write(int deviceID, std::string content) {
+    static int printer_write(int deviceID, std::string content)
+    {
         return Device_Printer::printer_write(deviceID, content);
     }
-    static int terminal_write(int deviceID, std::string content) {
+    static int terminal_write(int deviceID, std::string content)
+    {
         return Device_Terminal::terminal_write(deviceID, content);
     }
 };
